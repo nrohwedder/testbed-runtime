af4ce4320b4ce43ffb774f1f5ad9ee1f65771c34
Soenke Nommensen <nommensen@itm.uni-luebeck.de>
2010-10-28 15:53:04 +0200

    Initial commit.

---
+++ gwt-web-ui/client/src/main/java/de/uniluebeck/itm/webui/client/WebUi.java
@@ -0,0 +1,12 @@
+package de.uniluebeck.itm.webui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class WebUi implements EntryPoint {
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
